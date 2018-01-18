429160c48f7f22f6abbf72b1cdd41cc8c459932d
Justin <justin.xkcd@gmail.com>
2018-01-18 17:39:25 -0500

    added a sticky footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,9 +4,29 @@ import './App.css';
 
 class Footer extends Component{
   render(){
+    var style = {
+      // borderTop: "1px solid #E7E7E7",
+      textAlign: "center",
+      padding: "20px",
+      position: "fixed",
+      left: "0",
+      bottom: "0",
+      height: "60px",
+      width: "100%",
+    }
+
+    var phantom = {
+      display: 'block',
+      padding: '20px',
+      height: '60px',
+      width: '100%',
+    }
     return(
       <div>
-        
+        <div style={phantom} />
+          <div style={style}>
+            This is a sticky footer.
+        </div>
       </div>
     )
   }
