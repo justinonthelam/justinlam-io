d559573eac88790bcff8cc74a90c79ff32b3e51f
Justin <justin.xkcd@gmail.com>
2018-01-18 16:20:57 -0500

    basic file structure using react-router

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom'
+import './App.css';
+
+class Header extends Component{
+  render(){
+    return(
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default Header
---
+++ src/components/Home.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom'
+import './App.css';
+
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        Hello World
+      </div>
+    )
+  }
+}
+
+export default Home
